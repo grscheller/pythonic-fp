5a01a199cd489f09926a46ebb07ef42f73439678
grscheller <geoffrey@scheller.com>
2025-09-08 21:45:10 -0400

    Rebuilt pythonic-fp-fptools docstrings
    - for upcoming v5.1.0 PyPI release
    - TODO: restructure Sphinx docs to new format

--- docs/fptools/development/source/conf.py
+++ docs/fptools/development/source/conf.py
@@ -13,7 +13,7 @@
 project = 'Pythonic FP - Functional Programming Tools'
 copyright = '2023-2025, Geoffrey R. Scheller'
 author = 'Geoffrey R. Scheller'
-release = '5.0.1' 
+release = '5.1.0' 
 proposed_release_string = 'a proposed'
 # proposed_release_string = 'the'
 
