f04cec14f3636737e64bf38eaf72c6af8a1df207
grscheller <geoffrey@scheller.com>
2025-12-04 18:15:14 -0700

    Redid fptools development docs for a proposed 5.2.0 release.

--- docs/fptools/development/source/conf.py
+++ docs/fptools/development/source/conf.py
@@ -13,7 +13,7 @@
 project = 'Pythonic FP - Functional Programming Tools'
 copyright = '2023-2025, Geoffrey R. Scheller'
 author = 'Geoffrey R. Scheller'
-release = '5.1.3'
+release = '5.2.0'
 release_string = 'Proposed PyPI'
 # release_string = 'PyPI'
 
