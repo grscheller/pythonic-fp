2ff207c004d28399890b36c9bb41e42975d3261b
grscheller <geoffrey@scheller.com>
2026-05-03 20:56:46 -0600

    Fixed what went wrong last commit.
    Updated:
    
    - .github/workflows/static.yml
    - docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-# 
 # Update Release string to agree with pythonic-fp/pyproject.toml before
 # a PyPI release.
-#
 #
 
 project = 'Pythonic FP'
