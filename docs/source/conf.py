c383537d0dd56bc0ae16132158e9941cc31ef7c4
grscheller <geoffrey@scheller.com>
2026-09-12 07:24:08 -0600

    Updated .gitignore foe all pythonic-fp and boring-math projects

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# Update Release string to agree with pythonic-fp/pyproject.toml before
-# a PyPI release.
+# Update Release string to agree with pythonic-fp/pyproject.toml
+# before a PyPI release.
 #
 
 project = 'Pythonic FP'
@@ -21,11 +21,11 @@ templates_path = ['_templates']
 exclude_patterns: list[str] = []
 
 html_theme_options = {
-    "light_css_variables": {
-        "color-link--visited": "var(--color-link)",
+    'light_css_variables': {
+        'color-link--visited': 'var(--color-link)',
     },
-    "dark_css_variables": {
-        "color-link--visited": "var(--color-link)",
+    'dark_css_variables': {
+        'color-link--visited': 'var(--color-link)',
     },
 }
 html_theme = 'furo'
